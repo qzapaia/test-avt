01ce038e11a3803d17e4def64cfb423ffa51e75f
Marcelo Zapaia <marcelo.zapaia@avantrip.com>
2017-09-06 18:06:54 -0300

    improve stories decorators

--- src/stories.decorator.js
+++ src/stories.decorator.js
@@ -4,13 +4,8 @@ import addReadme from 'storybook-readme/with-readme'
 import { Provider as ApolloProvider } from './apollo-client'
 import { ThemeProvider } from 'styled-components'
 
-import _ from 'lodash';
-var p = _.wrap(_.escape, function(func, text) {
-  return '<p>' + func(text) + '</p>';
-});
-
 const createDecorator = config => (story,b,c,d) => {
-
+  
   const newStory = () => (
     <ApolloProvider>
       <ThemeProvider theme={config.theme}>
@@ -19,10 +14,7 @@ const createDecorator = config => (story,b,c,d) => {
     </ApolloProvider>
   )
 
-  const decorator = addReadme(config.readme);
-
-  return decorator(newStory,b,c,d);
-
+  return addReadme(config.readme)(newStory,b,c,d);
 }
 
 export default createDecorator;
