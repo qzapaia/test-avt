d6ce4f15fad67334d2ccd74471ed6bf97d59ca6e
Roberto Braile <roberto.braile@gmail.com>
2017-09-18 12:11:07 -0300

    add CheckoutItemsDetail component

---
+++ src/avantrip/CheckoutItemsDetail/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CheckoutItemsDetail = ({stages}) => (
+ (<div> 
+   {stages.map((stage, index) => (
+      <div key={index}>
+        <div>
+          <span>{stage.leg}</span>
+          <span>Duration <strong>{stage.duration}</strong></span>
+          <span>{stage.stops}</span>
+            {
+              stage.flights.map((flight, idx) => (
+                <div key={idx}>
+                  <div>
+                    <img src={flight.marketingCarrier.logo} alt="logo" />
+                    <span>{flight.marketingCarrier.name}</span>
+                  </div>
+                  <hr />
+                  <div>
+                    <div>
+                      <span>Vuelo N° {flight.number}</span>
+                      <span>Clase: {flight.class}</span>
+                    </div>
+                  </div>
+                  <hr />
+                  <div>
+                    <div>
+                      <p>Salida</p>
+                      <p>{flight.departure.airportCode}</p>
+                      <p>{flight.departure.location}</p>
+                      <p>{flight.departure.date}</p>
+                      <p>{flight.departure.hour}</p>
+                    </div>
+                    <div>
+                      <p>Llegada</p>
+                      <p>{flight.arrival.airportCode}</p>
+                      <p>{flight.arrival.location}</p>
+                      <p>{flight.arrival.date}</p>
+                      <p>{flight.arrival.hour}</p>
+                    </div>
+                  </div>
+                  <div>          
+                    {
+                      (flight.stopTime) ? `Escala en ${flight.arrival.location} con espera ${flight.stopTime}` : ''
+                    }
+                  </div>
+                </div>
+              ))
+            }
+        </div>
+        <div></div>
+      </div>
+    ))
+    }
+  </div>)
+)
+
+CheckoutItemsDetail.propTypes = {
+  stages: PropTypes.array.isRequired
+}
+
+
+export default CheckoutItemsDetail;
---
+++ src/avantrip/CheckoutItemsDetail/stories.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import CheckoutItemsDetail from './';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withState, compose } from 'recompose';
+
+import generalDecorator from '../../stories.decorator.js';
+
+import theme from '../styled.theme';
+import readme from './README.md';
+
+let stages = [{
+  leg: 'IDA',
+  duration: '19 hs 10 m',
+  stops: '1 escala',
+  flights: [
+    {
+      marketingCarrier: {
+        name: "LATAM Airlines",
+        logo: 'https://cdn.avantrip.com/static/images/airlines-icons/LA.png'
+      },
+      number: 456,
+      class: 'Economica',
+      departure: {
+        airportCode: 'AEP',
+        location: 'Buenos Aires',
+        date: 'Sáb 18 Nov De 2017',
+        hour: '12:45 hs'
+      },
+      arrival: {
+        airportCode: 'SCL',
+        location: 'Santiago',
+        date: 'Sáb 18 Nov De 2017',
+        hour: '15:45 hs'
+      },
+      stopTime: '8hs 10m'
+    },
+    {
+      marketingCarrier: {
+        name: "LATAM Airlines",
+        logo: 'https://cdn.avantrip.com/static/images/airlines-icons/LA.png'
+      },
+      number: 500,
+      class: 'Economica',
+      departure: {
+        airportCode: 'SCL',
+        location: 'Santiago',
+        date: 'Sáb 18 Nov De 2017',
+        hour: '23:20 hs'
+      },
+      arrival: {
+        airportCode: 'MIA',
+        location: 'Miami',
+        date: 'Dom 19 Nov De 2017',
+        hour: '05:55 hs'
+      }
+    }
+  ]
+},
+{
+  leg: 'VUELTA',
+  duration: '19 hs 10 m',
+  stops: 'Directo',
+  flights: [
+    {
+      marketingCarrier: {
+        name: "LATAM Airlines",
+        logo: 'https://cdn.avantrip.com/static/images/airlines-icons/LA.png'
+      },
+      number: 7823,
+      class: 'Economica',
+      departure: {
+        airportCode: 'MIA',
+        location: 'Miami',
+        date: 'Sáb 25 Nov De 2017',
+        hour: '19:55 hs'
+      },
+      arrival: {
+        airportCode: 'EZE',
+        location: 'Buenos Aires',
+        date: 'Dom 26 Nov De 2017',
+        hour: '06:45 hs'
+      }
+    }
+  ]
+}];
+
+const enhace = withState('counter','increment',0);
+const CheckoutItemsDetailWithState =  enhace((props) => {
+  return (
+    <CheckoutItemsDetail stages={stages} />
+  )
+})
+
+storiesOf('avantrip/CheckoutItemsDetail', module)
+  .addDecorator(generalDecorator({
+    readme,
+    theme
+  }))
+  .add('Default', () => (
+    <CheckoutItemsDetailWithState></CheckoutItemsDetailWithState>
+  ))
+
