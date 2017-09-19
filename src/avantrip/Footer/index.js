29f70d90ecc905d07d073a37ccf2ff1501580d86
Roberto Braile <roberto.braile@gmail.com>
2017-09-19 16:56:24 -0300

    add Footer component

---
+++ src/avantrip/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Footer = () => (
+ <footer>
+   <h1>Esto es un pie(por favor insertar zapatilla)</h1>
+</footer>
+)
+
+export default Footer;
