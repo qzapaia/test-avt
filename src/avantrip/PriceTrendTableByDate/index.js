fc1234bd395eadd6d2bf8be1b886dc951a345133
Roque <roque.arganaraz@avantrip.com>
2017-09-20 15:55:04 -0300

    Tabs vs Spaces

--- src/avantrip/PriceTrendTableByDate/index.js
+++ src/avantrip/PriceTrendTableByDate/index.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 
 
 const itemStyle = {
-	width: '90px',
+  width: '90px',
   padding: '10px',
   backgroundColor: 'white',
   height: '60px',
@@ -16,7 +16,7 @@ const itemStyle = {
 }
 
 const selectedDateItemStyle = {
-	width: '90px',
+  width: '90px',
   padding: '10px',
   backgroundColor:'lightblue',
   height: '60px',
@@ -28,7 +28,7 @@ const selectedDateItemStyle = {
 }
 
 const bestDateItemStyle = {
-	width: '90px',
+  width: '90px',
   padding: '10px',
   backgroundColor:'green',
   height: '60px',
@@ -40,141 +40,141 @@ const bestDateItemStyle = {
 }
 
 const addFirstColumnHeader = flights => (
-	_.map(flights, ( fRow, key ) => {
-		//Unshift no devuelve el mismo array
-		fRow.unshift({
-			'rowTitle': 'VUELTA',
-			'day': moment(key).format('dddd'),
-			'date': moment(key).format('DD/MM/YYYY'),
-			'rawDate': key
-		});
-		return fRow; 
-	})
+  _.map(flights, ( fRow, key ) => {
+    //Unshift no devuelve el mismo array
+    fRow.unshift({
+      'rowTitle': 'VUELTA',
+      'day': moment(key).format('dddd'),
+      'date': moment(key).format('DD/MM/YYYY'),
+      'rawDate': key
+    });
+    return fRow; 
+  })
 ) 
 
 const addFirstRowHeader = ( flightGroupedByDepartureDate , flightsWithColumnHeader )  => {
 
-	const firstColumnFlightMatrix = 
-		_.map(flightGroupedByDepartureDate, ( fCol, key ) => (
-			{
-				'rowTitle': 'IDA',
-				'day': moment(key).format('dddd'),
-				'date': moment(key).format('DD/MM/YYYY'),
-				'rawDate': key
-			})
-		)
+  const firstColumnFlightMatrix = 
+    _.map(flightGroupedByDepartureDate, ( fCol, key ) => (
+      {
+        'rowTitle': 'IDA',
+        'day': moment(key).format('dddd'),
+        'date': moment(key).format('DD/MM/YYYY'),
+        'rawDate': key
+      })
+    )
 
-	firstColumnFlightMatrix.unshift({}); //Primer item de la matriz vacío
+  firstColumnFlightMatrix.unshift({}); //Primer item de la matriz vacío
 
-	flightsWithColumnHeader.unshift(firstColumnFlightMatrix);
+  flightsWithColumnHeader.unshift(firstColumnFlightMatrix);
 
-	return flightsWithColumnHeader;
+  return flightsWithColumnHeader;
 }
 
 const groupByDepartureDate = flights => {
-	return _.groupBy(flights, 'ida');
+  return _.groupBy(flights, 'ida');
 } 
 
 const groupByArrivalDate = flights => {
-	return _.groupBy(flights, 'vuelta');
+  return _.groupBy(flights, 'vuelta');
 } 
 
 const addMinPriceFlag = flights => {
-	const flightWithMinPrice = _.minBy(flights, 'price');
-	return _.reduce(flights, ( acc, f ) => {
-		if(!acc){acc=[]};
-		//Pregunto de nuevo si hay algún otro vuelo con el precio mínimo
-		if(f.price == flightWithMinPrice.price){
-			acc.push(f);
-		}
-		return acc;
-	}, [])
+  const flightWithMinPrice = _.minBy(flights, 'price');
+  return _.reduce(flights, ( acc, f ) => {
+    if(!acc){acc=[]};
+    //Pregunto de nuevo si hay algún otro vuelo con el precio mínimo
+    if(f.price == flightWithMinPrice.price){
+      acc.push(f);
+    }
+    return acc;
+  }, [])
 }
 
 const getItemStyleBy = (arrivalDate, departureDate, bestFlights, currentFlight) => {
-	if(bestFlights.length > 0 && bestFlights[0].price == currentFlight.price){
-		return bestDateItemStyle;
-	} else if(arrivalDate == currentFlight.vuelta && departureDate == currentFlight.ida){
-		return selectedDateItemStyle; 
-	} else if(arrivalDate == currentFlight.rawDate){
-		return selectedDateItemStyle; 
+  if(bestFlights.length > 0 && bestFlights[0].price == currentFlight.price){
+    return bestDateItemStyle;
+  } else if(arrivalDate == currentFlight.vuelta && departureDate == currentFlight.ida){
+    return selectedDateItemStyle; 
+  } else if(arrivalDate == currentFlight.rawDate){
+    return selectedDateItemStyle; 
 } else if(departureDate == currentFlight.rawDate){
-		return selectedDateItemStyle; 
-	} else {
-		return itemStyle;
-	}
+    return selectedDateItemStyle; 
+  } else {
+    return itemStyle;
+  }
 }
 
 const PriceTrendTableByDate = ({flightDates, selectedArrivalDate, selectedDepartureDate, onClick}) => {
 
-	const flightDatesWithMinPrices = addMinPriceFlag(flightDates);
+  const flightDatesWithMinPrices = addMinPriceFlag(flightDates);
 
-	const flightDatesMatrix = 
-		addFirstRowHeader(
-			groupByArrivalDate(flightDates),
+  const flightDatesMatrix = 
+    addFirstRowHeader(
+      groupByArrivalDate(flightDates),
 
-			addFirstColumnHeader(
-				groupByDepartureDate(flightDates)
-			)
-		)
+      addFirstColumnHeader(
+        groupByDepartureDate(flightDates)
+      )
+    )
 
-	return (
-	  <div>
-	  	{
-	  		_.map(flightDatesMatrix, fRow => (
-					<div style={{display:'flex'}}>
-	  				{
-		  				fRow.map( fColumn => (
-		  						<div>
-		  							{ fColumn.rowTitle && 
-		  								<div style={getItemStyleBy(
-		  										selectedArrivalDate,
-		  										selectedDepartureDate,
-		  										{},
-		  										fColumn
-		  									)}>
-				  							<div>
-				  								{fColumn.rowTitle}
-				  							</div>
-				  							<div>
-				  								{fColumn.day}
-				  							</div>
-												<div>
-				  								{fColumn.date}
-				  							</div>
-				  						</div>
-		  							}
+  return (
+    <div>
+      {
+        _.map(flightDatesMatrix, fRow => (
+          <div style={{display:'flex'}}>
+            {
+              fRow.map( fColumn => (
+                  <div>
+                    { fColumn.rowTitle && 
+                      <div style={getItemStyleBy(
+                          selectedArrivalDate,
+                          selectedDepartureDate,
+                          {},
+                          fColumn
+                        )}>
+                        <div>
+                          {fColumn.rowTitle}
+                        </div>
+                        <div>
+                          {fColumn.day}
+                        </div>
+                        <div>
+                          {fColumn.date}
+                        </div>
+                      </div>
+                    }
 
-		  							{ !fColumn.rowTitle &&	
-				  						<div onClick={ e => onClick(fColumn)} style={getItemStyleBy(
-				  								selectedArrivalDate, 
-				  								selectedDepartureDate,
-				  								flightDatesWithMinPrices,
-				  								fColumn
-				  							)}>
+                    { !fColumn.rowTitle &&  
+                      <div onClick={ e => onClick(fColumn)} style={getItemStyleBy(
+                          selectedArrivalDate, 
+                          selectedDepartureDate,
+                          flightDatesWithMinPrices,
+                          fColumn
+                        )}>
 
-				  							
-				  							{ (fColumn.rowTitle || fColumn.price) &&
-				  								<div>
-						  							<div>Desde</div>
-						  							<div>
-						  								<span>ARS</span>&nbsp;<span>{fColumn.price}</span>
-						  							</div>
-					  							</div>
-				  							}
+                        
+                        { (fColumn.rowTitle || fColumn.price) &&
+                          <div>
+                            <div>Desde</div>
+                            <div>
+                              <span>ARS</span>&nbsp;<span>{fColumn.price}</span>
+                            </div>
+                          </div>
+                        }
 
-				  						</div>
-				  					}
-				  				</div>
-		  					)
-	  					)
-		  			}
-	  			</div>	
-	  		))
-	  	}
-	  </div>
-	  
-	)
+                      </div>
+                    }
+                  </div>
+                )
+              )
+            }
+          </div>  
+        ))
+      }
+    </div>
+    
+  )
 }
 
 PriceTrendTableByDate.propTypes = {
