8fa7dc692f59d4ece42014a14916cb1ffabc720c
Tomas Diaco <tomas.diaco@avantrip.com>
2017-09-20 10:40:32 -0300

    Se agrego el Text component al label con un type M

--- src/avantrip/InputCheckbox/container.styled.js
+++ src/avantrip/InputCheckbox/container.styled.js
@@ -4,7 +4,6 @@ import image from './image'
 export const InputContainer = styled.div`
 
 `
-
 export const InputStyle = styled.div`
   display: inline-block;
   width: 22px;
@@ -14,19 +13,16 @@ export const InputStyle = styled.div`
   vertical-align: top;
 
 `
-
 export const Input = styled.input`
 position: absolute;
 left: -100%;
   & ~ ${InputStyle} {
-    ${'' /* Si el input está en checked cambia el background del InputStyle */}
     background: url(${image});
     ${props => props.type == 'checkbox' && props.checked ? 'background-position: 0 -132px' : props.type == 'checkbox' ? 'background-position: 0 -88px' : ''};
     ${props => props.type == 'radio' && props.checked ? 'background-position: 0 -44px' : props.type == 'radio' ? 'background-position: 0 -22px' : ''};
     ${props => props.disabled ? 'opacity: 0.75' : ''};
   }
 `
-
 export const LabelContainer = styled.label`
   display: inline-flex;
   flex-wrap: wrap;
