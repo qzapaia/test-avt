f218d7791bc899a81cc95920423707e2149e58fc
Marcelo Zapaia <marcelo.zapaia@avantrip.com>
2017-09-20 15:49:56 -0300

    server compression

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const compression = require('compression')
 const PORT = process.env.PORT || 3899;
 
+app.use(compression())
+
 app.use('/storybook',express.static('storybook-static'));
-app.use('/',(req,res)=>res.redirect('/storybook'));
+
+app.use('/', function(req,res){
+  res.redirect('/storybook');
+});
 
 app.listen(PORT,()=>console.log(`running on ${PORT}`))
