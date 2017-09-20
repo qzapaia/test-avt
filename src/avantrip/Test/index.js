843e25b062752575a024de888643e223476452cc
Marcelo Zapaia <marcelo.zapaia@avantrip.com>
2017-09-20 14:23:27 -0300

    merge master

--- src/avantrip/Test/index.js
+++ src/avantrip/Test/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
 
-const Test = ({text, onClick}) => (
-  <div onClick={onClick}>
-=======
 import styled from 'styled-components';
-import image from './image';
 
 const Cont = styled.div`
   background-color: blue;
-  background-image: url(${image});
   width: 200px;
   height: 200px;
 `
 
-
 const Test = ({text, onClick}) => (
   <div onClick={onClick}>
     <Cont></Cont>
->>>>>>> master
     Test component
     <br/>
     Counter {text}
--- src/avantrip/Text/index.js
+++ src/avantrip/Text/index.js
@@ -2,15 +2,9 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const StyledText = styled.span`
-<<<<<<< HEAD
-  font-size: ${props=>props.theme.sizes[props.type]};
-  font-weight: ${props=>types[props.type].weight};
-  font-family: ${props=>types[props.type].family};
-=======
   font-size: ${props=>props.theme.texts[props.type].size};
   font-weight: ${props=>props.theme.texts[props.type].weight};
   font-family: ${props=>props.theme.texts[props.type].family};
->>>>>>> master
   color: ${props=>props.theme.colors[props.color] || 'inherit'};
 `
 StyledText.propTypes = {
