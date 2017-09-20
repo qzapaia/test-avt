a030d851cbd2e894f745c834ad85c9cd1a07103b
Tomas Diaco <tomas.diaco@avantrip.com>
2017-09-20 11:37:39 -0300

    reset de estilos x default del <ul>

--- src/avantrip/List/container.styled.js
+++ src/avantrip/List/container.styled.js
@@ -4,6 +4,8 @@ const ContainerList = styled.ul`
   display: flex;
   flex-direction: ${props=>props.type == 'list' ? 'column' : 'row'};
   flex-wrap: ${props=>props.type == 'list' ? 'nowrap' : 'wrap'};
+  padding: 0;
+  margin: 0;
+  list-style: none;
 `
-
 export default ContainerList;
