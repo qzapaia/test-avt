fba6caac14ab441ef2551c59f38ec2222f37e18e
Tomas Diaco <tomas.diaco@avantrip.com>
2017-10-02 16:35:51 -0300

    select basic style

--- src/global/Select/index.js
+++ src/global/Select/index.js
@@ -1,4 +1,38 @@
-import GlobalSelect from '../../global/Select';
+import React from 'react';
 import PropTypes from 'prop-types';
+import ReactSelect from 'react-select';
+import {SelectContainer} from './styled'
+import selectCSS from 'react-select/dist/react-select.css';
+import { injectGlobal } from "styled-components";
+injectGlobal`${selectCSS}`;
 
-export default GlobalSelect;
+const Select = (props) => {
+  const {
+    placeholder,
+    name,
+    options,
+    onChange,
+    value
+  } = props;
+
+  return (
+    <ReactSelect
+      {...props}
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+      options={options}
+    />
+  )
+}
+Select.propTypes = {
+  onChange: PropTypes.func
+}
+
+Select.defaultProps = {
+  searchable:false
+}
+
+
+export default Select;
