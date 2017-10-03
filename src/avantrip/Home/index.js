3f860b1a0ba9fcd8c92db93033e118a53dc8341c
Tomas Diaco <tomas.diaco@avantrip.com>
2017-10-03 12:17:17 -0300

    agency text added

--- src/avantrip/Home/index.js
+++ src/avantrip/Home/index.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import FlightSearchBox from '../FlightSearchBox/withData';
 // import Slider from '../FlightSearchBox/withData';
 import Slider from '../Slider';
-import {MainSection, MaxWidth, FlightSearchBoxAbsolute} from './styled';
+import Text from '../Text';
+import {MainSection, MaxWidth, FlightSearchBoxAbsolute, AgencyInfo, AgencyTitle, AgencyText} from './styled';
 
 const Home = ({
   text,
@@ -21,16 +22,27 @@ const Home = ({
             title='busca tu vuelo'
           />
         </FlightSearchBoxAbsolute>
-        <Slider>
-            <img src="https://placeholdit.co//i/1440x465?&bg=e2432d&fc=fffff&text=Slide" />
-            <img src="https://placeholdit.co//i/1440x465?&bg=e2432d&fc=fffff&text=Slide2" />
-            <img src="https://placeholdit.co//i/1440x465?&bg=e2432d&fc=fffff&text=Slide3" />
-            <img src="https://placeholdit.co//i/1440x465?&bg=e2432d&fc=fffff&text=Slide4" />
-            <img src="https://placeholdit.co//i/1440x465?&bg=e2432d&fc=fffff&text=Slide5" />
-        </Slider>
 
       </MaxWidth>
+      <Slider>
+        <img src="https://placeholdit.co//i/1440x465?&bg=e2432d&fc=fffff&text=Slide" />
+        <img src="https://placeholdit.co//i/1440x465?&bg=e2432d&fc=fffff&text=Slide2" />
+        <img src="https://placeholdit.co//i/1440x465?&bg=e2432d&fc=fffff&text=Slide3" />
+        <img src="https://placeholdit.co//i/1440x465?&bg=e2432d&fc=fffff&text=Slide4" />
+        <img src="https://placeholdit.co//i/1440x465?&bg=e2432d&fc=fffff&text=Slide5" />
+      </Slider>
     </MainSection>
+
+
+    <AgencyInfo>
+      <AgencyTitle tag='h1' type='xxl'>
+        Avantrip.com: Agencia de turismo y viajes
+      </AgencyTitle>
+      <AgencyText tag='p'>
+        Somos una <strong>agencia de viajes</strong> con años de experiencia en la industria turística. En Avantrip.com podés elegir entre <strong>cientos de vuelos</strong> a destinos de todo el mundo, <strong>encontrar tu hotel ideal, seleccionar paquetes turísticos a medida</strong>, disfrutar de la mejor oferta de <strong>cruceros por el mundo</strong> y mucho más. Experimentá la facilidad de <strong>pagar en cuotas con todos los bancos</strong> y al precio más bajo con la mejor <strong>agencia de turismo</strong> de Argentina! Viví una experiencia de compra rápida, segura, fácil y empezá a disfrutar tus <strong>vacaciones con Avantrip.com</strong>. Ante cualquier duda o inconveniente podés llamar a nuestro centro de atención al cliente y recibir una respuesta rápida y satisfactoria.
+      </AgencyText>
+    </AgencyInfo>
+
     <div>Resto del contenido</div>
   </div>
 )
--- src/avantrip/Home/styled.js
+++ src/avantrip/Home/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Text from '../Text';
 
 export const MainSection = styled.section`
   display: block;
@@ -17,4 +18,22 @@ export const MaxWidth = styled.div`
   margin: 0 auto;
   max-width: ${props=> props.theme.viewport.desktop};
   position: relative;
+`
+export const AgencyInfo = styled.article`
+  margin: 15px auto;
+  max-width: ${props=> props.theme.viewport.desktop};
+  position: relative;
+`
+export const AgencyTitle = Text.extend`
+  margin: 10px 0;
+  font-weight: 300;
+  color: ${props=> props.theme.colors.darkergray};
+  
+`
+export const AgencyText = Text.extend`
+  color: ${props=> props.theme.colors.darkergray};
+  line-height: 20px;
+  strong{
+    font-weight: 400;
+  }
 `
