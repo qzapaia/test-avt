99d89316e4ddf731fa4de067eeefd6fe815d5ca0
AleG <alejandro.garcia@avantrip.com>
2017-10-05 18:02:38 -0300

    Correcciones

--- src/global/PriceTrendCalendar/actions.js
+++ src/global/PriceTrendCalendar/actions.js
@@ -1,11 +1,5 @@
-export const SET_HISTOGRAM_DATA = 'SET_HISTOGRAM_DATA';
 export const SET_SELECTED_MONTH = 'SET_SELECTED_MONTH';
 export const SET_SELECTED_DAY = 'SET_SELECTED_DAY';
-
-export const setData = data => ({
-  type:SET_HISTOGRAM_DATA,
-  payload:data
-})
 
 export const setSelectedMonth = selectedMonth => ({
   type:SET_SELECTED_MONTH,
