ed222fc3f4494407f8aa4840cf71065fcf6a1f01
Marcelo Zapaia <marcelo.zapaia@avantrip.com>
2017-10-05 16:59:37 -0300

    modify and add new generators to plop

--- src/global/Test/actions.js
+++ src/global/Test/actions.js
@@ -1,11 +1,11 @@
-export const SET_DATA = 'SET_DATA';
-export const SET_REPOS = 'SET_REPOS';
+export const SET_REPOS = 'TEST_SET_REPOS';
 
-export const getData = id => (
-  fetch(`https://api.com/${id}`)
-  .then(res=>res.json())
-  .then(data=>dispatch(setData(data)))
-)
+export const getData = id => async dispatch => {
+  const res = await fetch('https://api.github.com/users/qzapaia/repos');
+  const data = await res.json();
+  dispatch(setData(data));
+  dispatch({type:'TEST_ACTION'});
+}
 
 export const setData = data => ({
   type:SET_REPOS,
