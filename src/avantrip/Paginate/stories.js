67177b9f4efee0e528337446a9e84ae2d09abb37
Alan Landolfi <alandolfi@macasignar-acbc32798cdf.intranet.biblostravel.com>
2017-10-09 16:01:00 -0300

    add query graph in withdata

--- src/avantrip/Paginate/stories.js
+++ src/avantrip/Paginate/stories.js
@@ -16,6 +16,21 @@ import { random } from 'lodash';
 
 const randomPagesCount = random(20);
 
+let search = {
+  origin: "BUE",
+  destination: "COR",
+  departureDate: "11-03-2018",
+  returningDate: "20-03-2018",
+  passengers: {
+    adults: 1,
+    children: 0,
+    infants:0
+  },
+  cabinClass: "Economy",
+  channel: "DESKTOP",
+  portal:"AVANTRIP"
+}
+
 storiesOf('avantrip/Paginate', module)
   .addDecorator(
     generalDecorator({
@@ -27,5 +42,18 @@ storiesOf('avantrip/Paginate', module)
     })
   )
   .add('Default', () => (
-    <PaginateWithData pagesCount={randomPagesCount} ></PaginateWithData>
+    <Paginate pagesCount={randomPagesCount} ></Paginate>
+  ))
+  .add('With Data',()=> (
+    <PaginateWithData 
+      origin={search.origin}
+      destination={search.destination}
+      departureDate={search.departureDate}
+      returningDate={search.returningDate}
+      passengersAdults={search.passengers.adults}
+      passengersChildren= {search.passengers.children}
+      passengersInfants={search.passengers.infants}
+      cabinClass={search.cabinClass}
+      channel={search.channel}
+      portal={search.portal} />
   ))
--- src/global/Paginate/withData.js
+++ src/global/Paginate/withData.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { gql, graphql } from 'react-apollo';
 import { connect } from "react-redux";
 import Paginate from "./";
+
+import { get } from 'lodash';
 
 import { setPagesCount, setSelectedPage } from "./actions";
 
@@ -24,5 +27,51 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     }
   }
 }
+
+const PaginateWithData = (props) => {
+  return <Paginate pagesCount={0}/>
+}
   
-export default connect(mapStateToProps, mapDispatchToProps)(Paginate);
+// GrahQL Query
+export const query = gql`
+query PaginationQuery(
+  $origin: String!,
+  $destination: String!,
+  $departureDate: String!,
+  $returningDate: String!,
+  $passengersAdults: Int!,
+  $passengersChildren: Int!,
+  $passengersInfants: Int!,
+  $cabinClass: FligthCabinClassInput!,
+  $channel: String!,
+  $portal: String!)
+  {
+    orchestrator{
+      availability{
+        roundtrip(origin:$origin,destination:$destination,departureDate:$departureDate,returningDate:$returningDate,
+          passengers:{ adults:$passengersAdults,children:$passengersChildren,infants:$passengersInfants },cabinClass:$cabinClass,channel:$channel,portal:$portal){
+          metas,references
+        }
+      }
+    }
+  }`
+
+  const WithApolloComponentPaginate = graphql(query,{
+    options: props => ({
+      variables : {
+        origin : props.origin,
+        destination : props.destination,
+        departureDate : props.departureDate,
+        returningDate : props.returningDate,
+        passengersAdults : props.passengersAdults,
+        passengersChildren : props.passengersChildren,
+        passengersInfants : props.passengersInfants,
+        cabinClass : props.cabinClass,
+        channel : props.channel,
+        portal : props.portal, 
+      },
+    })
+  })(PaginateWithData);
+
+const WithDataComponent = connect(mapStateToProps, mapDispatchToProps)(WithApolloComponentPaginate);
+export default WithDataComponent;
