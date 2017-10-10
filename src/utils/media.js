f1ceefff40b5f34e494bf8b595c2a33ba915a318
Tomas Diaco <tomas.diaco@avantrip.com>
2017-10-10 10:49:24 -0300

    Revert "Revert "product card withData added""
    This reverts commit 827ced9df0a20c974c30616d642046c2bf4e5738.

--- src/utils/media.js
+++ src/utils/media.js
@@ -3,7 +3,7 @@ export const getSize = () => {
 
   return (w <= 375 && { size:0,  size_name:'s' }) ||
          (w <= 425 && { size:1,  size_name:'m' }) ||
-         (w <= 768 && { size:2,  size_name:'l' }) ||
+         (w <= 990 && { size:2,  size_name:'l' }) ||
          (w <= 1280 && { size:3,  size_name:'xl' }) ||
          (w > 1280 && { size:4,  size_name:'xxl' })
          null;
