1a92bd75ec5d936ab00af6b055cc804d4a988c44
Tomas Diaco <tomas.diaco@avantrip.com>
2017-10-19 16:32:57 -0300

    Revert "fixing styles"
    This reverts commit 261d62fd296d46a75c655595d0746bc446f8eef7.

--- src/global/Breadcrumb/styled.js
+++ src/global/Breadcrumb/styled.js
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
 
-export const ContainerBreadcrumb = styled.ul`
+const ContainerBreadcrumb = styled.ul`
   display: flex;
   flex-direction: 'row';
   flex-wrap: 'wrap'};
 `
-export const Arrow = styled.span`
-  margin: 0 5px;
-  font-size: 10px;
-`
+
+export default ContainerBreadcrumb;
